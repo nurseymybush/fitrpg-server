751f08d38c88eb3b4066a8a7b21325f442ce7ee6
Chance Nursey-Bush <cnurseyb@asu.edu>
2017-07-06 10:21:38 -0700

    undo try

--- server/item/item_controllers.js
+++ server/item/item_controllers.js
@@ -41,8 +41,8 @@ module.exports = exports = {
   post : function(req, res, next) {
     var $promise = Q.nbind(Item.save, Item);
     $promise(req.body.item)
-      .then(function (_id) {
-        res.send(_id);
+      .then(function (id) {
+        res.send(id);
       })
       .fail(function (reason) {
         next(reason);
